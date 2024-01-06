57f1d30e14052f407a1e3e2f541887b3e84bc093
TheViperShow <thevipershowita@gmail.com>
2024-01-07 00:02:31 +0100

    First commit. Project structure added.

---
+++ src/main/java/top/cmarco/fireworksmechanics/FireworksMechanics.java
@@ -0,0 +1,14 @@
+package top.cmarco.fireworksmechanics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FireworksMechanics extends JavaPlugin {
+
+    /**
+     * Startup method for this plugin.
+     */
+    @Override
+    public void onEnable() {
+
+    }
+}
